3e4df24d0c0045d23f308e30d897b5bbe4b9f645
mbecker20 <becker.maxh@gmail.com>
2021-02-02 02:49:50 -0800

    have to instantiate useCenterMenu in each project

--- src/CenterMenu/hooks.ts
+++ src/CenterMenu/hooks.ts
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import { AddMenu, CloseMenu, OpenMenu } from "../menuManager/types";
 
-export function useCenterMenu(name: string, mmAddMenu: AddMenu, mmCloseMenu: CloseMenu, onOpen?: OpenMenu, save = true, onClose?: () => void) {
-  const [exists, setExists] = useState(false)
-  mmAddMenu(name, (...args: any) => {
-    if (onOpen) onOpen(...args)
-    setExists(true)
-  })
-  return {
-    cmControlData: exists,
-    closeMenu: () => {
-      setExists(false)
-      if (onClose) onClose()
-      if (save) mmCloseMenu()
-    },
-    softClose: () => {
-      setExists(false)
-      if (onClose) onClose()
+export function makeUseCenterMenu(mmAddMenu: AddMenu, mmCloseMenu: CloseMenu) {
+  return function useCenterMenu(name: string, onOpen?: OpenMenu, save = true, onClose?: () => void) {
+    const [exists, setExists] = useState(false)
+    mmAddMenu(name, (...args: any) => {
+      if (onOpen) onOpen(...args)
+      setExists(true)
+    })
+    return {
+      cmControlData: exists,
+      closeMenu: () => {
+        setExists(false)
+        if (onClose) onClose()
+        if (save) mmCloseMenu()
+      },
+      softClose: () => {
+        setExists(false)
+        if (onClose) onClose()
+      }
     }
   }
 }
