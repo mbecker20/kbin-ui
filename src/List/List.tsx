7227e119a463f2632b3dd34f75d07fd0b2af3fda
mbecker20 <becker.maxh@gmail.com>
2021-02-08 06:48:01 -0800

    noItemsText to List

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -5,10 +5,13 @@ import LabelledItems from '../LabelledItems/LabelledItems'
 import Map from '../Map'
 import ListItem from './ListItem'
 import CSS from 'csstype'
+import Conditional from '../Conditional'
+import Text from '../Text/Text'
 
-function List<T>({ array, map, selectedIndex, label, style, itemStyle }: {
+function List<T>({ array, map, noItemsText, selectedIndex, label, style, itemStyle }: {
   array: T[]
   map: (t: T, i: number) => ReactNode
+  noItemsText: string
   selectedIndex?: number
   label?: ReactNode
   style?: CSS.Properties
@@ -21,6 +24,9 @@ function List<T>({ array, map, selectedIndex, label, style, itemStyle }: {
         <LabelledItems
           label={label}
         >
+          <Conditional showIf={array.length === 0}>
+            <Text text={noItemsText} style={{ alignSelf: 'center' }} />
+          </Conditional>
           <FlexCol alignItems='stretch' style={style}>
             <Map
               array={array}
