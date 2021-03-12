1242c9ac1c3222dd303e19bfa0ae947f305a59f5
mbecker20 <becker.maxh@gmail.com>
2021-03-12 06:04:36 -0800

    choose alignment

--- src/DropdownMenu/DropdownMenu.tsx
+++ src/DropdownMenu/DropdownMenu.tsx
@@ -7,9 +7,20 @@ import useJSS from './style'
 import CSS from 'csstype'
 import { useReRenderOnResize } from '../hooks'
 
+function getLeft(rect: DOMRect, alignment?: 'center' | 'right' | 'left') {
+  switch (alignment) {
+    case 'center': return (rect.right + rect.left) / 2
+    case 'right': return rect.right
+    case 'left': return rect.left
+    default: return (rect.right + rect.left) / 2
+  }
+  return 
+}
+
 function DropdownMenu({ 
   children, alt, title, margin ,padding, width, height,
-  backgroundColor, menuPadding, menuStyle, svgStyle, menuBorderRadius
+  backgroundColor, menuPadding, menuStyle, svgStyle, menuBorderRadius,
+  alignment
 }: {
   children: ReactNode
   alt: string
@@ -23,13 +34,15 @@ function DropdownMenu({
   menuStyle?: CSS.Properties
   svgStyle?: CSS.Properties
   menuBorderRadius?: string
+  alignment?: 'center' | 'right' | 'left'
 }) {
   const [open, setOpen] = useState(false)
   const svgRef = useRef<HTMLImageElement>(null)
   const boundingRect = svgRef.current ? svgRef.current.getBoundingClientRect() : undefined
   const classes = useJSS({
     top: svgRef.current && boundingRect ? svgRef.current.offsetTop + boundingRect.bottom - boundingRect.top : 0,
-    left: boundingRect ? (boundingRect.right + boundingRect.left) / 2 : 0
+    left: boundingRect ? getLeft(boundingRect, alignment) : 0,
+    alignment
   })
   useReRenderOnResize()
   return (
--- src/DropdownMenu/style.ts
+++ src/DropdownMenu/style.ts
@@ -1,11 +1,20 @@
 import { createUseStyles } from 'react-jss'
 
+function getAlignment(alignment?: 'center' | 'right' | 'left') {
+  switch (alignment) {
+    case 'center': return '-50%'
+    case 'right': return '-100%'
+    case 'left': return '0%'
+    default: return '-50%'
+  }
+}
+
 const useJSS = createUseStyles({
   DropdownMenu: {
     position: 'fixed',
     top: ({ top }) => top,
     left: ({ left }) => left,
-    transform: 'translate(-50%, 0%)'
+    transform: ({ alignment }) => `translate(${getAlignment(alignment)}, 0%)`
   },
 })
 
