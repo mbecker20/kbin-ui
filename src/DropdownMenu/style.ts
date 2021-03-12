69b0859d31107e99b341b38133c3cde93c331fbb
mbecker20 <becker.maxh@gmail.com>
2021-03-12 05:49:23 -0800

    fix

--- src/DropdownMenu/style.ts
+++ src/DropdownMenu/style.ts
@@ -2,7 +2,7 @@ import { createUseStyles } from 'react-jss'
 
 const useJSS = createUseStyles({
   DropdownMenu: {
-    position: 'sticky',
+    position: 'fixed',
     top: ({ top }) => top,
     left: ({ left }) => left,
     transform: 'translate(-50%, 0%)'
